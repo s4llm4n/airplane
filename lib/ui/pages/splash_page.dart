dfc296df542fe61935e0d17a0bd789945f32aba1
s4llm4n <s4llm4n@gmail.com>
2022-12-27 06:22:24 +0700

    first commit

---
+++ lib/ui/pages/splash_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_plane.png',
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
