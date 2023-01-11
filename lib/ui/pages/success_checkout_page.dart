a169d8939d30aeb819adf483c7335c1553317205
s4llm4n <s4llm4n@gmail.com>
2023-01-11 14:14:02 +0700

    commit checkout

---
+++ lib/ui/pages/success_checkout_page.dart
@@ -0,0 +1,52 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:airplane/ui/widgets/custom_button.dart';
+import 'package:flutter/material.dart';
+
+class SuccessCheckoutPage extends StatelessWidget {
+  const SuccessCheckoutPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 150,
+              margin: EdgeInsets.only(bottom: 80),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage(
+                      'assets/image_success.png',
+                      ),
+                    ),
+                  ),
+                ),
+                Text(
+                  'Well Booked 😍',
+                  style: blackTextStyle.copyWith(
+                    fontSize: 32,
+                    fontWeight: semiBold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Are you ready to explore the new world of experiences?',
+                  style: greyTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: light,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                CustomButton(title: title, onPressed: onPressed)
+              ],
+          ),
+        ),
+      );
+  }
+}
