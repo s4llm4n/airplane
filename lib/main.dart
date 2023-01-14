c8d25704c53ab82b34f9743a29bfc2a1921b4e34
s4llm4n <s4llm4n@gmail.com>
2023-01-14 18:10:00 +0700

    commit gradd

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'ui/pages/splash_page.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
