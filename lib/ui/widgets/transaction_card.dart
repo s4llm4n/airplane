c8874516e3911484c9fc3e1f5c38b5b9f7a84bb1
s4llm4n <s4llm4n@gmail.com>
2023-01-16 15:23:53 +0700

    commit card_page

---
+++ lib/ui/widgets/transaction_card.dart
@@ -0,0 +1,155 @@
+import 'package:airplane/models/transaction_model.dart';
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'booking_details_item.dart';
+
+class TransactionCard extends StatelessWidget {
+  final TransactionModel transaction;
+
+  const TransactionCard(this.transaction, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30),
+      padding: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 30,
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(18),
+        color: kWhiteColor,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // NOTE: Destination Tile
+          Row(
+            children: [
+              Container(
+                width: 70,
+                height: 70,
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      transaction.destination.imageUrl,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transaction.destination.name,
+                      style: blackTextStyle.copyWith(
+                        fontSize: 18,
+                        fontWeight: medium,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      transaction.destination.city,
+                      style: greyTextStyle.copyWith(
+                        fontWeight: light,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 21,
+                    height: 21,
+                    margin: EdgeInsets.only(right: 2),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(
+                          'assets/icon_star.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Text(
+                    transaction.destination.rating.toString(),
+                    style: blackTextStyle.copyWith(
+                      fontWeight: medium,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+
+          // NOTE: Booking Details Text
+          Container(
+            margin: EdgeInsetsDirectional.only(top: 20),
+            child: Text(
+              'Booking Details',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+          ),
+
+          // NOTE: Booking Details Items
+          BookingDetailsItem(
+            title: 'Traveler',
+            valueText: '${transaction.amountOfTravel} person',
+            valueColor: kBlackColor,
+          ),
+          BookingDetailsItem(
+            title: 'Seat',
+            valueText: transaction.selectedSeat,
+            valueColor: kBlackColor,
+          ),
+          BookingDetailsItem(
+            title: 'Insurance',
+            valueText: transaction.insurance ? 'YES' : 'NO',
+            valueColor: transaction.insurance ? kGreenColor : kRedColor,
+          ),
+          BookingDetailsItem(
+            title: 'Refundable',
+            valueText: transaction.refundable ? 'YES' : 'NO',
+            valueColor: transaction.refundable ? kGreenColor : kRedColor,
+          ),
+          BookingDetailsItem(
+            title: 'VAT',
+            valueText: '${(transaction.vat * 100).toStringAsFixed(0)}%',
+            valueColor: kBlackColor,
+          ),
+          BookingDetailsItem(
+            title: 'Price',
+            valueText: NumberFormat.currency(
+              locale: 'id',
+              symbol: 'IDR',
+              decimalDigits: 0,
+            ).format(transaction.price),
+            valueColor: kBlackColor,
+          ),
+          BookingDetailsItem(
+            title: 'Grand Total',
+            valueText: NumberFormat.currency(
+              locale: 'id',
+              symbol: 'IDR',
+              decimalDigits: 0,
+            ).format(transaction.grandTotal),
+            valueColor: kPrimaryColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
